02a6c39616982505ed78e2bf95fb5b1e024a8a42
jeetiss <jeetiss@yandex.ru>
2019-08-12 20:44:42 +0300

    focus

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import { useForm } from './form'
 import { useAnalitycs } from './ga'
@@ -7,18 +7,23 @@ import './app.css'
 const validate = (values) => values.text && values !== ''
 
 export default () => {
+  const area = useRef()
   const send = useAnalitycs()
   const submit = (values, reset) => {
     send({ ec: 'send form', ea: values.text })
     reset()
   }
 
+  useEffect(() => {
+    area.current.focus()
+  }, [])
+
   const { bindForm, bind, isValid } = useForm(submit, validate)
 
   return (
     <form {...bindForm()}>
       <div className='container'>
-        <textarea {...bind('text')} />
+        <textarea ref={area} {...bind('text')} />
 
         <button disabled={!isValid}>Send</button>
       </div>
