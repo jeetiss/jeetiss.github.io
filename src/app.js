18d3a69c1caeb755336a836d3d693ba3af4489b7
jeetiss <jeetiss@yandex.ru>
2019-08-12 11:28:30 +0300

    setup form

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
 import React from 'react'
 
-// trigger deploy
-export default () => <div>Hello world</div>
+import { useForm } from './form'
+import { useAnalitycs } from './ga'
+import './app.css'
+
+const validate = (values) => values.text && values !== ''
+
+export default () => {
+  const send = useAnalitycs()
+  const submit = (values, reset) => {
+    send(values)
+    reset()
+  }
+
+  const { bindForm, bind, isValid } = useForm(submit, validate)
+
+  return (
+    <form {...bindForm()}>
+      <div className='container'>
+        <textarea {...bind('text')} />
+
+        <button disabled={!isValid}>Send</button>
+      </div>
+    </form>
+  )
+}
---
+++ src/ga.js
@@ -0,0 +1,10 @@
+import { createContext, useContext, useCallback } from 'react'
+import GA from 'ganalytics'
+
+const context = createContext(new GA('UA-126675501-1'))
+
+export const useAnalitycs = () => {
+  const ga = useContext(context)
+
+  return useCallback((data) => ga.send('event', data), [ga])
+}
