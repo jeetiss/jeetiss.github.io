ff11454f08a80dd41a67c5c89629521cb13fefc1
jeetiss <jeetiss@yandex.ru>
2020-02-18 22:19:14 +0300

    implement simplified bull alg

--- src/sockets.js
+++ src/sockets.js
@@ -9,6 +9,9 @@ const channel = name => {
   const chl = channels.subscribe(name)
 
   return {
+    destroy: () => {
+      chl.disconnect()
+    },
     on: (event, handler) => {
       chl.bind(event, handler)
     },
@@ -30,37 +33,117 @@ const channel = name => {
   }
 }
 
+// export default () => {
+//   let tid
+//   const myId = id()
+//   let clients = new Set([myId])
+
+//   const sender = channel('value')
+
+//   sender.on('get', ({ idOfInit }) => {
+//     console.log(`node with ${idOfInit} starts counting`)
+
+//     if (idOfInit !== myId) {
+//       clearTimeout(tid)
+//       console.log(`sending my id`)
+//       sender.emit('set', { id: myId, idOfInit })
+//     }
+//   })
+
+//   sender.on('set', ({ id, idOfInit }) => {
+//     console.log(`recive node ${id} from ${idOfInit}`)
+//     if (idOfInit === myId) {
+//       clients.add(id)
+//     }
+//   })
+
+//   setTimeout(() => {
+//     clients = new Set([myId])
+//     sender.emit('get', { idOfInit: myId })
+
+//     tid = setTimeout(() => {
+//       console.log('finish counting')
+//       console.log('size: ', clients.size)
+//     }, 5000)
+//   })
+// }
+
+const getStatus = (id, election, coordinatorId, ping, status) => `
+  <div>ID: ${id}</div>
+  <div>EL: ${election ? '+' : '-'}</div>
+  <div>CID: ${coordinatorId}</div>
+  <div>PING: ${ping}</div>
+  <div>STAT: ${typeof status === 'string' ? status : 'conecting'}</div>
+`
+
+const getPing = (pid) => new Promise((resolve, reject) => {
+  const start = Date.now()
+  const transport = channel(`ping-${pid}`)
+  transport.on('pong', () => {
+    resolve(Date.now() - start)
+    transport.destroy()
+  })
+  transport.emit('pong')
+})
+
 export default () => {
-  let tid
-  const myId = id()
-  let clients = new Set([myId])
+  const div = document.querySelector('body main div')
 
-  const sender = channel('value')
+  const transport = channel('sockets')
+  const pid = id()
+  let election = false
+  let coordinatorId = null
+  let electionTimeout
 
-  sender.on('get', ({ idOfInit }) => {
-    console.log(`node with ${idOfInit} starts counting`)
+  getPing(pid).then((ping) => {
+    transport.on('election', (data) => {
+      if (data.pid === pid) return
 
-    if (idOfInit !== myId) {
-      clearTimeout(tid)
-      console.log(`sending my id`)
-      sender.emit('set', { id: myId, idOfInit })
-    }
-  })
+      clearTimeout(electionTimeout)
 
-  sender.on('set', ({ id, idOfInit }) => {
-    console.log(`recive node ${id} from ${idOfInit}`)
-    if (idOfInit === myId) {
-      clients.add(id)
-    }
-  })
+      election = true
+      coordinatorId = null
 
-  setTimeout(() => {
-    clients = new Set([myId])
-    sender.emit('get', { idOfInit: myId })
+      if (data.pid < pid) {
+        transport.emit('ok', { pid })
+      }
 
-    tid = setTimeout(() => {
-      console.log('finish counting')
-      console.log('size: ', clients.size)
-    }, 5000)
+      div.innerHTML = getStatus(pid, election, coordinatorId, ping)
+    })
+
+    coordinatorId = pid
+    transport.emit('election', { pid })
+    electionTimeout = setTimeout(() => {
+      transport.emit('coordinator', { pid })
+    }, ping * 3)
+
+    div.innerHTML = getStatus(pid, election, coordinatorId, ping)
+
+    transport.on('ok', data => {
+      if (election) return
+
+      if (coordinatorId < data.pid) {
+        coordinatorId = data.pid
+      }
+
+      clearTimeout(electionTimeout)
+      electionTimeout = setTimeout(() => {
+        transport.emit('coordinator', { pid: coordinatorId })
+      }, ping * 2)
+
+      div.innerHTML = getStatus(pid, election, coordinatorId, ping)
+    })
+
+    transport.on('coordinator', ({ pid: winnerPid }) => {
+      coordinatorId = winnerPid
+      election = false
+
+      div.innerHTML = getStatus(pid, election, coordinatorId, ping, coordinatorId === pid ? 'master' : 'fork')
+      if (coordinatorId === pid) {
+        console.log('master')
+      } else {
+        console.log('fork')
+      }
+    })
   })
 }
