78ca4e2b23382fee946532b512005838c7ba3f1a
jeetiss <jeetiss@yandex.ru>
2020-02-21 00:10:59 +0300

    wtf code

--- src/sockets.js
+++ src/sockets.js
@@ -9,14 +9,33 @@ const getStatus = (id, election, coordinatorId, ping, status) => `
   <div>STAT: ${typeof status === 'string' ? status : 'conecting'}</div>
 `
 
-const getPing = (pid) => new Promise((resolve, reject) => {
+const run = arr => arr.forEach(fn => fn())
+
+const pingMaster = (pid, transport) => new Promise((resolve, reject) => {
+  let id
+  let ping
   const start = Date.now()
-  const transport = channel(`ping-${pid}`)
-  transport.on('pong', () => {
-    resolve(Date.now() - start)
-    transport.destroy()
-  })
-  transport.trigger('pong')
+  const subs = [
+    transport.on('ping', (data) => {
+      if (data.pid === pid) {
+        console.log(Date.now() - start)
+        ping = Math.max(Date.now() - start, 900)
+
+        id = setTimeout(() => {
+          run(subs)
+          resolve({ ping })
+        }, Math.round(ping * 1.5))
+      }
+    }),
+
+    transport.on('pong', ({ pid }) => {
+      run(subs)
+      clearTimeout(id)
+      resolve({ ping, cid: pid })
+    })
+  ]
+
+  transport.trigger('ping', { pid })
 })
 
 export default () => {
@@ -27,12 +46,53 @@ export default () => {
   let election = false
   let coordinatorId = null
   let electionTimeout
+  let forkTimeout
+  let restartTimeout
+  let unsub = () => void 0
 
-  getPing(pid).then((ping) => {
+  pingMaster(pid, transport).then(({ ping, cid }) => {
+    const startElection = () => {
+      clearTimeout(restartTimeout)
+      clearTimeout(forkTimeout)
+      clearTimeout(electionTimeout)
+      unsub()
+      election = false
+      coordinatorId = pid
+      transport.trigger('election', { pid })
+      electionTimeout = setTimeout(() => {
+        transport.trigger('coordinator', { pid })
+      }, ping * 3)
+    }
+
+    const fork = () => {
+      console.log('fork')
+
+      const trigger = () => {
+        forkTimeout = setTimeout(() => {
+          transport.trigger('ping', { pid })
+
+          restartTimeout = setTimeout(() => {
+            startElection()
+          }, ping * 4)
+        }, Math.round(10000 + Math.random() * 20000))
+      }
+
+      unsub()
+      unsub = transport.on('pong', () => {
+        clearTimeout(restartTimeout)
+        clearTimeout(forkTimeout)
+        trigger()
+      })
+      trigger()
+    }
+
     transport.on('election', (data) => {
       if (data.pid === pid) return
 
+      unsub()
       clearTimeout(electionTimeout)
+      clearTimeout(restartTimeout)
+      clearTimeout(forkTimeout)
 
       election = true
       coordinatorId = null
@@ -41,16 +101,22 @@ export default () => {
         transport.trigger('ok', { pid })
       }
 
+      electionTimeout = setTimeout(() => {
+        startElection()
+      }, ping * 6)
+
       div.innerHTML = getStatus(pid, election, coordinatorId, ping)
     })
 
-    coordinatorId = pid
-    transport.trigger('election', { pid })
-    electionTimeout = setTimeout(() => {
-      transport.trigger('coordinator', { pid })
-    }, ping * 3)
+    if (cid) {
+      coordinatorId = cid
+      div.innerHTML = getStatus(pid, election, coordinatorId, ping, coordinatorId === pid ? 'master' : 'fork')
+      fork()
+    } else {
+      startElection()
 
-    div.innerHTML = getStatus(pid, election, coordinatorId, ping)
+      div.innerHTML = getStatus(pid, election, coordinatorId, ping)
+    }
 
     transport.on('ok', data => {
       if (election) return
@@ -68,14 +134,20 @@ export default () => {
     })
 
     transport.on('coordinator', ({ pid: winnerPid }) => {
+      clearTimeout(electionTimeout)
       coordinatorId = winnerPid
       election = false
 
       div.innerHTML = getStatus(pid, election, coordinatorId, ping, coordinatorId === pid ? 'master' : 'fork')
       if (coordinatorId === pid) {
         console.log('master')
+
+        unsub()
+        unsub = transport.on('ping', () => {
+          transport.trigger('pong', { pid })
+        })
       } else {
-        console.log('fork')
+        fork()
       }
     })
   })
