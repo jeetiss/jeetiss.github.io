b76293347c6a47f05f8e2d7cb4c678dde4266623
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-11-14 21:02:00 +0300

    fix gtm

--- app/(legacy)/layout.js
+++ app/(legacy)/layout.js
@@ -1,13 +1,11 @@
+import Script from "next/script";
+
 export default function PageLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <link href="main.css" rel="stylesheet"></link>
-        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PGDX74FW');`}</script>
+        <Script src="gtm.js" strategy="beforeInteractive" />
       </head>
 
       <body>{children}</body>
